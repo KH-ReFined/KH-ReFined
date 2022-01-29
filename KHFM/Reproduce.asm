dcb7fb6ddcc8dadcb12d3005ab3b139340a683ce
Topaz Whitelock <topaztk@yahoo.com>
2022-01-29 19:17:44 +0300

    Create Reproduce.asm with 2 mods.

---
+++ KHFM/Reproduce.asm
@@ -0,0 +1,25 @@
+/*
+=================================================
+      KINGDOM HEARTS - RE:FIXED FOR 1 FM!
+       COPYRIGHT TOPAZ WHITELOCK - 2022
+ LICENSED UNDER MIT. GIVE CREDIT WHERE IT'S DUE! 
+=================================================
+*/
+
+### ALL OF THESE MUST BE DONE PRE-PATCH ###
+
+-> Cutscene Limiter Removal
+
+VERSION 0.0:
+    @ 0x16C199 => [89 0D 2D FD 1C 02] to [90 90 90 90 90 90]
+VERSION 0.1:
+    @ 0x16C349 => [89 0D 7D FB 1C 02] to [90 90 90 90 90 90]
+
+-> Cutscene Skip Fix
+
+VERSION 0.0:
+    @ 0x1752AC => [0F 94 C0 C3] to [0F 9E C0 C3]
+    @ 0x17A0F3 => [74 10] to [7E 10]
+VERSION 0.1:
+    @ 0x175452 => [0F 94 C0 C3] to [0F 9E C0 C3]
+    @ 0x17A2A0 => [74 10] to [7E 10]
