bc727ee4bb2c42f9c3b98f310b9791f803f6d04c
Topaz Whitelock <95656963+TopazTK@users.noreply.github.com>
2022-01-29 06:38:12 +0300

    Fix formatting.

--- KHFM/Functions.cs
+++ KHFM/Functions.cs
@@ -14,61 +14,61 @@ namespace ReFixed
 {
 	public static class Functions
 	{
-        public static void OverrideText()
-        {
-            // Config Text
+		public static void OverrideText()
+		{
+		    // Config Text
 
-            if (Hypervisor.Read<byte>(Variables.FovTextAddresses[1]) != 0x30)
-            {
-                for (int i = 0; i < Variables.FovTextOffsets.Length; i++)
-                    Hypervisor.Write<ushort>(Variables.FovTextAddresses[0] + (0x02 * i), Variables.FovTextOffsets[i]);
-                
-                for (int i = 0; i < Variables.CamTextOffsets.Length; i++)
-                    Hypervisor.Write<ushort>(Variables.CamTextAddresses[0] + (0x02 * i), Variables.CamTextOffsets[i]);
+		    if (Hypervisor.Read<byte>(Variables.FovTextAddresses[1]) != 0x30)
+		    {
+			for (int i = 0; i < Variables.FovTextOffsets.Length; i++)
+			    Hypervisor.Write<ushort>(Variables.FovTextAddresses[0] + (0x02 * i), Variables.FovTextOffsets[i]);
 
-                Hypervisor.WriteArray(Variables.FovTextAddresses[1], Variables.FovTextArray);
-                Hypervisor.WriteArray(Variables.CamTextAddresses[1], Variables.CamTextArray);
-            }
-        }
+			for (int i = 0; i < Variables.CamTextOffsets.Length; i++)
+			    Hypervisor.Write<ushort>(Variables.CamTextAddresses[0] + (0x02 * i), Variables.CamTextOffsets[i]);
 
-        public static void OverrideFov()
-        {
-            var _fovToggle = Hypervisor.Read<int>(Variables.FovSwitchAddress);
+			Hypervisor.WriteArray(Variables.FovTextAddresses[1], Variables.FovTextArray);
+			Hypervisor.WriteArray(Variables.CamTextAddresses[1], Variables.CamTextArray);
+		    }
+		}
 
-            switch(_fovToggle)
-            {
-                case 0:
-                {
-                    var _fovFirst = Hypervisor.Read<float>(Variables.FovAddresses[0]);
+		public static void OverrideFov()
+		{
+		    var _fovToggle = Hypervisor.Read<int>(Variables.FovSwitchAddress);
 
-                    if (_fovFirst != 400F)
-                    {
-                        for (int i = 0; i < Variables.FovAddresses.Length; i++)
-                            Hypervisor.Write<float>(Variables.FovAddresses[i], Variables.FovClassic[i]);
-                    }
+		    switch(_fovToggle)
+		    {
+			case 0:
+			{
+			    var _fovFirst = Hypervisor.Read<float>(Variables.FovAddresses[0]);
 
-                    break;
-                }
+			    if (_fovFirst != 400F)
+			    {
+				for (int i = 0; i < Variables.FovAddresses.Length; i++)
+				    Hypervisor.Write<float>(Variables.FovAddresses[i], Variables.FovClassic[i]);
+			    }
 
-                case 1:
-                {
-                    var _fovFirst = Hypervisor.Read<float>(Variables.FovAddresses[0]);
+			    break;
+			}
 
-                    if (_fovFirst != 600F)
-                    {
-                        for (int i = 0; i < Variables.FovAddresses.Length; i++)
-                            Hypervisor.Write<float>(Variables.FovAddresses[i], Variables.FovEnhanced[i]);
-                    }
+			case 1:
+			{
+			    var _fovFirst = Hypervisor.Read<float>(Variables.FovAddresses[0]);
 
-                    break;
-                }
-            }
-        }
+			    if (_fovFirst != 600F)
+			    {
+				for (int i = 0; i < Variables.FovAddresses.Length; i++)
+				    Hypervisor.Write<float>(Variables.FovAddresses[i], Variables.FovEnhanced[i]);
+			    }
 
-        public static void Execute()
-        {
-            OverrideText();
-            OverrideFov();
-        }
-    }
+			    break;
+			}
+		    }
+		}
+
+		public static void Execute()
+		{
+		    OverrideText();
+		    OverrideFov();
+		}
+	}
 }
