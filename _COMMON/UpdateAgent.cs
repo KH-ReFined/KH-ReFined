413340b6a10ae6058ab3bdc3239ae69baafe785c
Topaz Whitelock <topaztk@yahoo.com>
2022-02-23 04:46:14 +0300

    Add an edge case for no internet.

--- _COMMON/UpdateAgent.cs
+++ _COMMON/UpdateAgent.cs
@@ -30,93 +30,109 @@ namespace ReFixed
         
         public static void UpdateCheck()
         {
-            var _gitClient = new GitHubClient(new ProductHeaderValue("ReFixed-Updater"));
-            var _latestInfo = _gitClient.Repository.Release.GetLatest("TopazTK", "KH-ReFixed").Result;
-
-            var _latestNumber = Convert.ToDouble(_latestInfo.TagName.Substring(1));
-            var _latestFile = _latestInfo.Assets[0].BrowserDownloadUrl;
-
-            var _downPath = Path.GetTempPath() + "reFixedUpdate.zip";
-            var _exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-
-            var _nameVersion = "[v{0}].exe";
-
-            var _exeAssembly = System.Reflection.Assembly.GetExecutingAssembly();
-            var _verInformation = System.Diagnostics.FileVersionInfo.GetVersionInfo(_exeAssembly.Location);
-            var _strVersion = _verInformation.FileVersion;
-
-            var _formatVersion = String.Format(_nameVersion, _strVersion);
-
-            if (_latestNumber > _version)
+            try
             {
-                var _boxMessage = "A new version of Re:Fixed has been detected!\n" +
-                                  "[Current: v{0}, Latest: v{1}]\n\n" +
-                                  "Do you wish to update the game?";  
+                var _gitClient = new GitHubClient(new ProductHeaderValue("ReFixed-Updater"));
+                var _latestInfo = _gitClient.Repository.Release.GetLatest("TopazTK", "KH-ReFixed").Result;
 
-                var _boxTitle = "Re:Fixed Updater";  
-                var _boxButtons = MessageBoxButtons.YesNo;  
+                var _latestNumber = Convert.ToDouble(_latestInfo.TagName.Substring(1));
+                var _latestFile = _latestInfo.Assets[0].BrowserDownloadUrl;
 
-                var _boxFormat = String.Format(_boxMessage, _version.ToString("0.00"), _latestNumber.ToString("0.00"));
+                var _downPath = Path.GetTempPath() + "reFixedUpdate.zip";
+                var _exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
 
-                var _boxResult = MessageBox.Show(_boxFormat, _boxTitle, _boxButtons, MessageBoxIcon.Information);  
+                var _nameVersion = "[v{0}].exe";
 
-                if (_boxResult == DialogResult.Yes)
+                var _exeAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+                var _verInformation = System.Diagnostics.FileVersionInfo.GetVersionInfo(_exeAssembly.Location);
+                var _strVersion = _verInformation.FileVersion;
+
+                var _formatVersion = String.Format(_nameVersion, _strVersion);
+
+                if (_latestNumber > _version)
                 {
-                    using (var _client = new WebClient())
+                    var _boxMessage = "A new version of Re:Fixed has been detected!\n" +
+                                    "[Current: v{0}, Latest: v{1}]\n\n" +
+                                    "Do you wish to update the game?";  
+
+                    var _boxTitle = "Re:Fixed Updater";  
+                    var _boxButtons = MessageBoxButtons.YesNo;  
+
+                    var _boxFormat = String.Format(_boxMessage, _version.ToString("0.00"), _latestNumber.ToString("0.00"));
+
+                    var _boxResult = MessageBox.Show(_boxFormat, _boxTitle, _boxButtons, MessageBoxIcon.Information);  
+
+                    if (_boxResult == DialogResult.Yes)
                     {
-                        _client.DownloadProgressChanged += onDownloadProgress;
-
-                        _client.DownloadFileAsync(new System.Uri(_latestFile), _downPath);
-
-                        var _downResult = _downForm.ShowDialog();
-
-                        if (_downResult != DialogResult.OK)
-                            _client.CancelAsync();
-
-                        else
+                        using (var _client = new WebClient())
                         {
-                            using (var _zipArch = ZipFile.Read(_downPath))
+                            _client.DownloadProgressChanged += onDownloadProgress;
+
+                            _client.DownloadFileAsync(new System.Uri(_latestFile), _downPath);
+
+                            var _downResult = _downForm.ShowDialog();
+
+                            if (_downResult != DialogResult.OK)
+                                _client.CancelAsync();
+
+                            else
                             {
-                                var _entryList = _zipArch.Entries;
-                                var _fileList = _entryList.Where(x => x.FileName.Contains(_formatVersion)).ToList();
-
-                                foreach (var _file in _fileList)
+                                using (var _zipArch = ZipFile.Read(_downPath))
                                 {
-                                    var _exeBase = AppDomain.CurrentDomain.BaseDirectory;
+                                    var _entryList = _zipArch.Entries;
+                                    var _fileList = _entryList.Where(x => x.FileName.Contains(_formatVersion)).ToList();
 
-                                    var _templateFM = _exeBase + "KINGDOM HEARTS {0} FINAL MIX.exe";
-                                    var _templateVN = _exeBase + "KINGDOM HEARTS {0}.exe";
+                                    foreach (var _file in _fileList)
+                                    {
+                                        var _exeBase = AppDomain.CurrentDomain.BaseDirectory;
 
-                                    var _extractName = _exeBase + "KINGDOM HEARTS FINAL MIX.exe";
+                                        var _templateFM = _exeBase + "KINGDOM HEARTS {0} FINAL MIX.exe";
+                                        var _templateVN = _exeBase + "KINGDOM HEARTS {0}.exe";
 
-                                    if (_file.FileName.Contains("Birth by Sleep"))
-                                        _extractName = String.Format(_templateFM, "Birth by Sleep");
+                                        var _extractName = _exeBase + "KINGDOM HEARTS FINAL MIX.exe";
 
-                                    if (_file.FileName.Contains("Chain of Memories"))
-                                        _extractName = String.Format(_templateVN, "Re_Chain of Memories");
+                                        if (_file.FileName.Contains("Birth by Sleep"))
+                                            _extractName = String.Format(_templateFM, "Birth by Sleep");
 
-                                    if (_file.FileName.Contains("Dream Drop Distance"))
-                                        _extractName = String.Format(_templateVN, "Dream Drop Distance");
+                                        if (_file.FileName.Contains("Chain of Memories"))
+                                            _extractName = String.Format(_templateVN, "Re_Chain of Memories");
 
-                                    if (_file.FileName.Contains("II"))
-                                        _extractName = String.Format(_templateFM, "II");
+                                        if (_file.FileName.Contains("Dream Drop Distance"))
+                                            _extractName = String.Format(_templateVN, "Dream Drop Distance");
 
-                                    if (!Directory.Exists(_exeBase + "BACKUP_EXE/"))
-                                        Directory.CreateDirectory(_exeBase + "BACKUP_EXE/");
+                                        if (_file.FileName.Contains("II"))
+                                            _extractName = String.Format(_templateFM, "II");
 
-                                    File.Move(_extractName, _extractName.Replace(_exeBase, _exeBase + "BACKUP_EXE/"));
+                                        if (!Directory.Exists(_exeBase + "BACKUP_EXE/"))
+                                            Directory.CreateDirectory(_exeBase + "BACKUP_EXE/");
 
-                                    using (FileStream _exeStream = new FileStream(_extractName, System.IO.FileMode.OpenOrCreate))
-                                        _file.Extract(_exeStream);
-                                }
-                            } 
+                                        File.Move(_extractName, _extractName.Replace(_exeBase, _exeBase + "BACKUP_EXE/"));
 
-                            File.Delete(_downPath);
-                            Process.Start(_exePath);
-                            System.Environment.Exit(0);
+                                        using (FileStream _exeStream = new FileStream(_extractName, System.IO.FileMode.OpenOrCreate))
+                                            _file.Extract(_exeStream);
+                                    }
+                                } 
+
+                                File.Delete(_downPath);
+                                Process.Start(_exePath);
+                                System.Environment.Exit(0);
+                            }
                         }
                     }
                 }
+            }
+
+            catch (Exception)
+            {
+                var _boxMessage = "Re:Fixed was not able to check for updates.\n" +
+                                  "Initializing the game normally...";  
+
+                var _boxTitle = "Re:Fixed Updater";  
+                var _boxButtons = MessageBoxButtons.OK;  
+
+                var _boxFormat = String.Format(_boxMessage, _version.ToString("0.00"), _latestNumber.ToString("0.00"));
+
+                var _boxResult = MessageBox.Show(_boxFormat, _boxTitle, _boxButtons, MessageBoxIcon.Error);  
             }
         }
 
