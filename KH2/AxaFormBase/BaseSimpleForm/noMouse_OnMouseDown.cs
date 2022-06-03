0f2c6fe656c6222a8dbcc5789606f4e10259788b
Topaz Whitelock <topaztk@yahoo.com>
2022-06-03 06:34:42 +0300

    More of that Cursor shits.

---
+++ KH2/AxaFormBase/BaseSimpleForm/noMouse_OnMouseDown.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+
+using Axa;
+
+namespace AxaFormBase
+{
+	public partial class BaseSimpleForm : Form
+	{
+		private void noMouse_OnMouseDown(object sender, MouseEventArgs e)
+		{
+			if (!_captureStatus && !_cursorHidden)
+			{
+				Cursor.Hide();
+
+			    _cursorHidden = true;		
+                _captureStatus = true;
+            }
+		}
+	}
+}
