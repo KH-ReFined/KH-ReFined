84a61bb4c6f355b57b59e7081d371c6a1cf086a9
Topaz Whitelock <topaztk@yahoo.com>
2022-06-27 01:08:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function changePage()
     document.title = "Kingdom Hearts - Re:Fixed | " + _fetchHash.substring(1).replace("_", " ");
 
     var xhr= new XMLHttpRequest();
+    xhr.open('GET', 'index.html', true);
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); 
     xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xhr.open('GET', 'index.html', true);
     xhr.onreadystatechange= function() {
         if (this.readyState!==4) return;
         if (this.status!==200) return; // or whatever error handling you want
