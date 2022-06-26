07ab17c235961f516ea9f3939457cb4d39565038
Topaz Whitelock <topaztk@yahoo.com>
2022-06-27 01:12:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ function changePage()
     var xhr= new XMLHttpRequest();
     xhr.withCredentials = true;
     xhr.open('GET', 'https://raw.githubusercontent.com/TopazTK/KH-ReFixed/website/index.html', true);
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); 
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
     xhr.onreadystatechange= function() {
         if (this.readyState!==4) return;
         if (this.status!==200) return; // or whatever error handling you want
