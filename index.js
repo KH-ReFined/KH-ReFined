b6781efaa95d950f70f66dad0f149a55a0a9a725
Topaz Whitelock <topaztk@yahoo.com>
2022-06-27 04:54:59 +0300

    [WEB] Let us see.

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ function changePage()
 
     _fetchHash = window.location.hash;
 
+    if (_fetchHash === "")
+    {
+        window.location.hash = "Main_Page";
+    }
+
     document.title = "Kingdom Hearts - Re:Fixed | " + _fetchHash.substring(1).replace("_", " ");
+
+    _currActive = document.querySelectorAll(".linkActive");
+    _inactive = document.querySelectorAll("a[href='" + _fetchHash + "']")[0];
+
+    for(var i = 0; i < _currActive.length; i++)
+    {
+        _currActive[i].classList.remove("linkActive");
+    }
+
+    _inactive.classList.add("linkActive");
+    _inactive.parentNode.children[0].className += " linkActive";
 
     fetch('https://raw.githubusercontent.com/TopazTK/KH-ReFixed/website/md/index.md')
     .then(response=> response.text())
--- md/index.md
+++ md/index.md
@@ -1,3 +1,9 @@
-# A New Step in KH Modding 
+# A Welcoming Message!
 
 Heya, this is TopazTK. If you are seeing this **work**, it means that the Markdown parser *finally* decided to do it's job!
+
+So yea, this is the official website for Re:Fixed, where all updates regarding it, now and future, will be posted and uploaded to for all of you to enjoy!
+
+I gotta admit, I am not a wizard of web development but eh, if I can make a nice lookin' website I sure am satisfied.
+
+Welp, nothing more to say for now, go over to the [Re:Fixed Twitter Page](https://twitter.com/KH_ReFixed) for more updates, or join the [Re:Fixed Discord Server](https://discord.gg/aGc7CyEJgv) for 24/7 interaction with others!
