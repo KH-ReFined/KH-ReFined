bc1d838fd210869f6a920c90737703f3c54d3586
Topaz Whitelock <topaz.whitelock@proton.me>
2026-02-17 16:06:28 +0100

    I swear I pushed this to code.

--- ReFined.KH2/src/dllmain.cpp
+++ ReFined.KH2/src/dllmain.cpp
@@ -100,6 +100,7 @@ char* BGM_WRITE_BUFFER;
 char* MENU_FNAME_BUFFER = ResolveRelativeAddress<char*>("\x48\x89\x74\x24\x10\x57\x48\x83\xEC\x20\x48\x8B\x05\x00\x00\x00\x00\x48\x8B\xF2\x48\x2B\xD0\x48\x8B\xF9\x66\x0F\x1F\x44\x00\x00\x44\x0F\xB6\x00\x0F\xB6\x0C\x10\x44\x2B\xC1", "xxxxxxxxxxxxx????xxxxxxxxxxxxxxxxxxxxxxxxxx", 0x0D);
 char* FAC_WRITE_BUFFER = ResolveRelativeAddress<char*>("\x48\x83\xEC\x68\x48\x8B\x05\x00\x00\x00\x00\x48\x33\xC4\x48\x89\x44\x24\x50\xE8\x00\x00\x00\x00\x84\xC0\x0F\x85\x00\x00\x00\x00\x38\x05\x00\x00\x00\x00\x0F\x85\x00\x00\x00\x00\x66\x83\x3D\x00\x00\x00\x00\x00", "xxxxxxx????xxxxxxxxx????xxxx????xx????xx????xxx????x", 0x1F9);
 
+
 // 3D Path Constructors.
 
 char* ConstructMDLX(char* objentryEntry, char* buff)
@@ -2291,6 +2292,10 @@ extern "C"
         if (IS_FASTBOOT)
             memcpy(YS::TITLE::Title2LD, "title_fast.2ld", 0x0E);
 
+        // If NOASPECT is active, remove the Aspect Handler.
+        if (IS_NOASPECT)
+            FUNCTION_ARRAY.erase("HANDLE_ASPECT");
+
         auto _addrGetMDLX = reinterpret_cast<char*>(YS::OBJENTRY::get_mdlx);
         auto _addrGetAPDX = reinterpret_cast<char*>(YS::OBJENTRY::get_apdx);
         auto _addrGetMSET = reinterpret_cast<char*>(YS::OBJENTRY::get_mset);
@@ -2814,6 +2819,9 @@ extern "C"
 
             DISCORD_ENABLED = _configStruct["General"]["discordRPC"] == "true" ? true : false;
 
+            if (!DISCORD_ENABLED)
+                FUNCTION_ARRAY.erase("DISCORD_RPC");
+
             ROOM_AMOUNT = atoi(_configStruct["General"]["saveRoomAmount"].c_str());
             SAVE_SLOT_OFFSET = atoi(_configStruct["General"]["saveSlot"].c_str());
 
@@ -3263,12 +3271,6 @@ extern "C"
             #if !defined(BUILD_ARCHIPELAGO) && !defined(BUILD_ARCHIPELAGO_LITE)
             for (auto _execPair : _execModule)
                 _execPair.second();
-
-            if (DISCORD_ENABLED && FUNCTION_ARRAY.find("DISCORD_RPC") != FUNCTION_ARRAY.end())
-                FUNCTION_ARRAY["DISCORD_RPC"]();
-
-            if (!IS_NOASPECT && FUNCTION_ARRAY.find("NO_ASPECT") != FUNCTION_ARRAY.end())
-                FUNCTION_ARRAY["HANDLE_ASPECT"]();
             #endif
         }
     }
