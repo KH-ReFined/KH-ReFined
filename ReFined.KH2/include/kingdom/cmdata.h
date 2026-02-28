a0071ac25860a906fe9ade56b53be37a83d481e3
Topaz Whitelock <topaz.whitelock@proton.me>
2026-02-28 19:07:33 +0100

    You know the drill

--- ReFined.KH2/include/kingdom/cmdata.h
+++ ReFined.KH2/include/kingdom/cmdata.h
@@ -12,6 +12,7 @@
 #include "area.h"
 #include "region.h"
 #include "cache_buff.h"
+#include "itempic.h"
 
 using namespace std;
 
@@ -22,9 +23,14 @@ extern "C"
         class DLL_EXPORT CmData
         {
         public:
+            static char call_statement[0x20];
+
             static char* MENU_FNAME_BUFFER;
+            static char* FAC_WRITE_BUFFER;
 
             static char* MakeFname(char* buff, char* fileName);
+            static void MakeFnameItempic(char* buff, uint16_t id);
+            static void MakeFnameFace(uint16_t id);
 
             struct staticInitializer
             {
@@ -33,6 +39,7 @@ extern "C"
                     printf("Handling hooks and redirections concerning Tz::CmData...\n\n");
 
                     MENU_FNAME_BUFFER = ResolveRelativeAddress<char*>("\x48\x89\x74\x24\x10\x57\x48\x83\xEC\x20\x48\x8B\x05\x00\x00\x00\x00\x48\x8B\xF2\x48\x2B\xD0\x48\x8B\xF9\x66\x0F\x1F\x44\x00\x00\x44\x0F\xB6\x00\x0F\xB6\x0C\x10\x44\x2B\xC1", "xxxxxxxxxxxxx????xxxxxxxxxxxxxxxxxxxxxxxxxx", 0x0D);
+                    FAC_WRITE_BUFFER = ResolveRelativeAddress<char*>("\x48\x83\xEC\x68\x48\x8B\x05\x00\x00\x00\x00\x48\x33\xC4\x48\x89\x44\x24\x50\xE8\x00\x00\x00\x00\x84\xC0\x0F\x85\x00\x00\x00\x00\x38\x05\x00\x00\x00\x00\x0F\x85\x00\x00\x00\x00\x66\x83\x3D\x00\x00\x00\x00\x00", "xxxxxxx????xxxxxxxxx????xxxx????xx????xx????xxx????x", 0x1F9);
 
                     vector<uint8_t> _absoluteInstructionJMP =
                     {
@@ -41,7 +48,7 @@ extern "C"
                     };
 
                     auto MakeFName_orig = SignatureScan<char*>("\x48\x89\x74\x24\x10\x57\x48\x83\xEC\x20\x48\x8B\x05\x00\x00\x00\x00\x48\x8B\xF2\x48\x2B\xD0\x48\x8B\xF9\x66\x0F\x1F\x44\x00\x00\x44\x0F\xB6\x00\x0F\xB6\x0C\x10\x44\x2B\xC1", "xxxxxxxxxxxxx????xxxxxxxxxxxxxxxxxxxxxxxxxx");
-                    uint64_t _constFunction = reinterpret_cast<uint64_t>(Tz::CmData::MakeFname);
+                    auto _constFunction = (uint64_t)MakeFname;
 
                     printf("Fetched Tz::CmData::MakeFname @ 0x%p\n", MakeFName_orig);
 
@@ -50,9 +57,37 @@ extern "C"
                     memcpy(_absoluteInstructionJMP.data() + 0x06, &_constFunction, 0x08);
                     memcpy(MakeFName_orig, _absoluteInstructionJMP.data(), _absoluteInstructionJMP.size());
 
-                    printf("Hooked Tz::CmData::MakeFname [0x%p] to Re:Fined function @ 0x%p\n", MakeFName_orig, Tz::CmData::MakeFname);
+                    printf("Hooked Tz::CmData::MakeFname [0x%p] to Re:Fined function @ 0x%p\n", MakeFName_orig, MakeFname);
 
-                    printf("\nSuccessfully handled Tz::CmData concerns.\n\n");
+                    auto _backReadImage = SignatureScan<char*>("\x48\x83\xEC\x68\x48\x8B\x05\x00\x00\x00\x00\x48\x33\xC4\x48\x89\x44\x24\x50\xE8\x00\x00\x00\x00\x84\xC0\x0F\x85\x00\x00\x00\x00\x38\x05\x00\x00\x00\x00\x0F\x85\x00\x00\x00\x00\x66\x83\x3D\x00\x00\x00\x00\x00", "xxxxxxx????xxxxxxxxx????xxxx????xx????xx????xxx????x");
+
+                    printf("Fetched Tz::UnitLoad::BackReadImage @ 0x%p\n", _backReadImage);
+
+                    vector<uint8_t> _patchBackReadImage =
+                    {
+                        0x48, 0x8B, 0xCC, // mov rcx, rsp
+                        0xFF, 0x15, 0x02, 0x00, 0x00, 0x00, 0xEB, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // call [someFunction] 
+                    };
+
+                    memset(_backReadImage + 0x7D, 0x90, 0x14);
+
+                    _constFunction = (uint64_t)MakeFnameItempic;
+
+                    memcpy(_patchBackReadImage.data() + 0x0B, &_constFunction, 0x08);
+                    memcpy(_backReadImage + 0x7D, _patchBackReadImage.data(), _patchBackReadImage.size());
+
+                    printf("Redirected subroutine Tz::UnitLoad::BackReadImage [0x%p] + 0x07D to Re:Fined function @ 0x%p\n", _backReadImage, MakeFnameItempic);
+
+                    memset(_backReadImage + 0x231, 0x90, 0x14);
+
+                    _constFunction = (uint64_t)MakeFnameFace;
+
+                    _patchBackReadImage[2] = 0xCD;
+
+                    memcpy(_patchBackReadImage.data() + 0x0B, &_constFunction, 0x08);
+                    memcpy(_backReadImage + 0x231, _patchBackReadImage.data(), _patchBackReadImage.size());
+
+                    printf("Redirected subroutine Tz::UnitLoad::BackReadImage [0x%p] + 0x231 to Re:Fined function @ 0x%p\n", _backReadImage, MakeFnameFace);
                 }
             };
 
--- ReFined.KH2/src/kingdom/cmdata.cpp
+++ ReFined.KH2/src/kingdom/cmdata.cpp
@@ -1,11 +1,15 @@
 #include "cmdata.h"
 
 char* Tz::CmData::MENU_FNAME_BUFFER = nullptr;
+char* Tz::CmData::FAC_WRITE_BUFFER = nullptr;
+
+char Tz::CmData::call_statement[0x20];
+
 Tz::CmData::staticInitializer Tz::CmData::initialize;
 
 char* Tz::CmData::MakeFname(char* buff, char* fileName)
 {
-    auto _fetchConfig = *reinterpret_cast<const uint16_t*>(YS::AREA::SaveData + 0x41A6);
+    auto _fetchConfig = *reinterpret_cast<const uint16_t*>(AREA::SaveData + 0x41A6);
     auto _regionPointer = (!YS::REGION::Get() || YS::REGION::Get() == 7) ? reinterpret_cast<char*>(*YS::REGION::DefaultRegion) : reinterpret_cast<char*>(*YS::REGION::Region);
 
     if (!strcmp(*reinterpret_cast<char**>(MENU_FNAME_BUFFER), fileName))
@@ -60,4 +64,59 @@ char* Tz::CmData::MakeFname(char* buff, char* fileName)
         printf("[Tz::CmData::MakeFname] | Fulfilling MENU request for: \"%s\"\n", buff);
         return buff;
     }
+}
+
+void Tz::CmData::MakeFnameFace(uint16_t id)
+{
+    auto _fetchConfig = *reinterpret_cast<const uint16_t*>(AREA::SaveData + 0x41A6);
+    auto _writeBuff = *reinterpret_cast<char**>(Tz::CmData::FAC_WRITE_BUFFER);
+
+    string _fetchFilePath(_writeBuff);
+
+    _fetchFilePath = _fetchFilePath.substr(0x04, _fetchFilePath.size() - 0x04);
+    _fetchFilePath = _fetchFilePath.substr(0x00, _fetchFilePath.size() - 0x04);
+
+    string _constructPath = _fetchConfig & 0x0200 ? "menu_2nd" : (_fetchConfig & 0x0400 ? "menu_3rd" : "menu");
+
+    _constructPath = _constructPath.append(_fetchFilePath);
+    _constructPath = _constructPath.append("%d.imd");
+
+    sprintf(_writeBuff, _constructPath.c_str(), id);
+
+    if (!YS::FILE::GetSize(_writeBuff))
+    {
+        _constructPath = "menu";
+
+        _constructPath = _constructPath.append(_fetchFilePath);
+        _constructPath = _constructPath.append("%d.imd");
+
+        sprintf(_writeBuff, _constructPath.c_str(), id);
+    }
+
+    printf("[Tz::CmData::MakeFnameFace] | Fulfilling FAC request for: \"%s\"\n", _writeBuff);
+}
+
+void Tz::CmData::MakeFnameItempic(char* buff, uint16_t id)
+{
+    buff = buff + 0x30;
+
+    auto _fetchPicturePtr = *YS::ITEMPIC::ToLoadID;
+    auto _fetchPictureID = 0x00;
+
+    if (_fetchPicturePtr != nullptr)
+        _fetchPictureID = *_fetchPicturePtr;
+
+    else
+        _fetchPictureID = id;
+
+    auto _fetchConfig = *reinterpret_cast<const uint16_t*>(AREA::SaveData + 0x41A6);
+    string _constructPath = _fetchConfig & 0x0200 ? "itempic_2nd/item-%03d.imd" : (_fetchConfig & 0x0400 ? "itempic_3rd/item-%03d.imd" : "itempic/item-%03d.imd");
+
+    sprintf(buff, _constructPath.c_str(), _fetchPictureID);
+
+    if (!YS::FILE::GetSize(buff))
+        sprintf(buff, "itempic/item-%03d.imd", _fetchPictureID);
+
+    printf("[Tz::CmData::MakeFnameItempic] | Fulfilling ITEMPIC request for: \"%s\"\n", buff);
+
 }
