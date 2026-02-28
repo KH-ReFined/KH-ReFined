6a72d13893d35e0373839448b7748f4df2ae32e5
Topaz Whitelock <topaz.whitelock@proton.me>
2026-02-28 19:07:58 +0100

    Debugging strings and a failsafe

--- ReFined.KH2/src/kingdom/voice.cpp
+++ ReFined.KH2/src/kingdom/voice.cpp
@@ -9,6 +9,8 @@ uint64_t* YS::VOICE::Part = ResolveRelativeAddress<uint64_t*>("\x85\xC9\x0F\x84\
 
 YS::FILE::ReadCallback YS::VOICE::ReadCallback = ResolveRelativeAddress<YS::FILE::ReadCallback>("\x85\xC9\x0F\x84\x93\x01\x00\x00\x41\x56\x48\x83\xEC\x70\x48\x8B", "xxxxxxxxxxxxxxxx", 0x138);
 
+YS::VOICE::staticInitializer YS::VOICE::initialize;
+
 void YS::VOICE::ReadEntryId(uint16_t entryID, char* buff)
 {
     auto _fetchConfig = *reinterpret_cast<const uint16_t*>(YS::AREA::SaveData + 0x41A6);
@@ -19,6 +21,8 @@ void YS::VOICE::ReadEntryId(uint16_t entryID, char* buff)
                            (_fetchConfig & 0x0020 ? "voice/bg/battle/%s%d_%s.win32.scd" : "voice/us/battle/%s%d_%s.win32.scd")));
 
     char _fileNameBuff[48];
+
+    fprintf(stdout, "[YS::VOICE::ReadEntryId] | Requested VOICE read for Object ID: 0x%X\n", entryID);
 
     if (entryID)
     {
@@ -44,6 +48,8 @@ void YS::VOICE::ReadEntryId(uint16_t entryID, char* buff)
 
                     if (_currentPart == _entryPart && _currentVoice == _areaVoice)
                     {
+                        fprintf(stdout, "[YS::VOICE::ReadEntryId] | Voice for Object ID 0x%X is already loaded. Aborting!\n", entryID);
+
                         *reinterpret_cast<uint16_t*>(YS::VOICE::Cache + _currentOffset) |= 1u;
                         return;
                     }
@@ -68,10 +74,18 @@ void YS::VOICE::ReadEntryId(uint16_t entryID, char* buff)
                 auto _worldName = YS::WORLD::GetName(YS::AREA::Current->World);
 
                 if (YS::REGION::Get() && YS::REGION::Get() != 0x07)
+                {
                     sprintf(_fileNameBuff, _constructPath.c_str(), _worldName, _areaVoice, YS::VOICE::Part[_entryPart]);
+
+                    if (!YS::FILE::GetSize(_fileNameBuff))
+                        sprintf(_fileNameBuff, "voice/us/battle/%s%d_%s.win32.scd", _worldName, _areaVoice, YS::VOICE::Part[_entryPart]);
+
+                }
 
                 else
                     sprintf(_fileNameBuff, "voice/jp/battle/%s%d_%s.win32.scd", _worldName, _areaVoice, YS::VOICE::Part[_entryPart]);
+
+                fprintf(stdout, "[YS::VOICE::ReadEntryId] | Currently processing file: %s\n", _fileNameBuff);
 
                 YS::FILE::ReadBack(_fileNameBuff, buff, YS::VOICE::ReadCallback, _queueOffset);
 
