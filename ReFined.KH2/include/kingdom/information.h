2938c7ec25acfcecd135188dcb9ada3f4f8975c7
Topaz Whitelock <topaz.whitelock@proton.me>
2026-04-11 11:52:29 +0200

    dk::NEXT_LEVEL::draw & dk::GET_MUNNY::draw

--- ReFined.KH2/include/kingdom/information.h
+++ ReFined.KH2/include/kingdom/information.h
@@ -21,5 +21,46 @@ extern "C"
 			using openGetMunny_t = void(*)(int munny);
 			static openGetMunny_t openGetMunny;
 		};
+
+		class DLL_EXPORT NEXT_EXP
+		{
+			public:
+				static void draw(char* nextExp);
+
+
+            struct staticInitializer
+            {
+                staticInitializer()
+                {
+                    printf("======================================================\n");
+                    printf("Handling hooks and redirections concerning dk::NEXT_EXP...\n\n");
+
+                    vector<uint8_t> _absoluteInstructionJMP =
+                    {
+                        0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,
+                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+                    };
+
+                    auto _constDraw = (uint64_t)draw;
+                    auto _draw_orig = MultiSignatureScan("\x40\x53\x55\x57\x48\x83\xEC\x30\x48\x8B\xE9\xE8\x00\x00\x00\x00", "xxxxxxxxxxxx????");
+
+                    printf("Fetched dk::NEXT_EXP::draw @ 0x%p and dk::GET_MUNNY::draw @ 0x%p\n", _draw_orig[0], _draw_orig[1]);
+
+                    memset(_draw_orig[0], 0x90, 0xBF);
+                    memset(_draw_orig[1], 0x90, 0xBF);
+
+                    memcpy(_absoluteInstructionJMP.data() + 0x06, &_constDraw, 0x08);
+                    memcpy(_draw_orig[0], _absoluteInstructionJMP.data(), _absoluteInstructionJMP.size());
+                    memcpy(_draw_orig[1], _absoluteInstructionJMP.data(), _absoluteInstructionJMP.size());
+
+                    printf("Hooked dk::NEXT_EXP::draw [0x%p] and dk::GET_MUNNY::draw [0x%p] to Re:Fined function @ 0x%p\n", _draw_orig[0], _draw_orig[1], draw);
+
+                    printf("\nSuccessfully handled dk::NEXT_EXP concerns.\n");
+                    printf("======================================================\n\n");
+                }
+            };
+
+            static staticInitializer initialize;
+		};
 	}
 }
