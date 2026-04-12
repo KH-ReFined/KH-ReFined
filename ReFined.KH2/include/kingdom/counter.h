55215f9b475e55aed3ff7771bbb175db432574ff
Topaz Whitelock <topaz.whitelock@proton.me>
2026-04-12 15:50:44 +0200

    WEGETTINTHEREWEGETTINTHEREWEGETTINTHEREWEGETTINTHEREWEGETTINTHEREWEGETTINTHEREWEGETTINTHERE

--- ReFined.KH2/include/kingdom/counter.h
+++ ReFined.KH2/include/kingdom/counter.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define DLL_EXPORT __declspec(dllexport)
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <cstdint>
 #include <Windows.h>
@@ -22,7 +23,39 @@ extern "C"
 			using forceLeave_t = void(*)(char* counter);
 			static forceLeave_t forceLeave;
 
-			static void update(char* counter);
+			static void update(char* counter); 
+
+            struct staticInitializer
+            {
+                staticInitializer()
+                {
+                    printf("======================================================\n");
+                    printf("Handling hooks and redirections concerning dk::COUNTER...\n\n");
+
+                    vector<uint8_t> _absoluteInstructionJMP =
+                    {
+                        0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,
+                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+                    };
+
+                    auto _updateFunc = (uint64_t)update;
+                    auto _update_orig = SignatureScan<char*>("\x48\x89\x5C\x24\x18\x48\x89\x74\x24\x20\x55\x57\x41\x56\x48\x8B\xEC\x48\x83\xEC\x50\x48\x8B\x05", "xxxxxxxxxxxxxxxxxxxxxxxx");
+
+                    printf("Fetched dk::COUNTER::update @ 0x%p\n", _update_orig);
+
+                    memset(_update_orig, 0x90, 0x38E);
+
+                    memcpy(_absoluteInstructionJMP.data() + 0x06, &_updateFunc, 0x08);
+                    memcpy(_update_orig, _absoluteInstructionJMP.data(), _absoluteInstructionJMP.size());
+
+                    printf("Hooked dk::COUNTER::update [0x%p] to Re:Fined function @ 0x%p\n", _update_orig, update);
+
+                    printf("\nSuccessfully handled dk::COUNTER concerns.\n");
+                    printf("======================================================\n\n");
+                }
+            };
+
+            static staticInitializer initialize;
 		};
 	}
 }
--- ReFined.KH2/src/kingdom/mission_count.cpp
+++ ReFined.KH2/src/kingdom/mission_count.cpp
@@ -1,3 +1,1 @@
 #include "mission_count.h"
-
-YS::MISSION_COUNT::destroy_t YS::MISSION_COUNT::destroy = ResolveRelativeAddress<YS::MISSION_COUNT::destroy_t>(reinterpret_cast<char*>(YS::LIMIT::destroy), 0xF9);
