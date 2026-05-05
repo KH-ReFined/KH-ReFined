2a46c6d1808e45532c731b16151b07152b937479
Topaz Whitelock <topaz.whitelock@proton.me>
2026-05-05 13:37:13 +0200

    [KH2] Make Anti Form and Final Form check for Sacrifice.

--- ReFined.KH2/src/kingdom/anti_form.cpp
+++ ReFined.KH2/src/kingdom/anti_form.cpp
@@ -32,7 +32,7 @@ int YS::ANTI_FORM::CheckForm(char* player, int form)
     mt19937 rng(dev());
     uniform_int_distribution<mt19937::result_type> randDist(1, 100);
 
-    if (randDist(rng) >= _calcAntiChance || *AREA::BattleStatus == 0x00)
+    if (randDist(rng) >= _calcAntiChance || *AREA::BattleStatus == 0x00 || YS::SACRIFICE::GetFormStatus(0x06))
         return form;
 
     auto _doesHaveFinal = YS::ITEM::GetNumBackyard(0x1D) || YS::SACRIFICE::GetFormStatus(0x05) || *(*reinterpret_cast<char**>(player + 0x5C0) + 0x1B1) < *(YS::COMMAND_ELEM::GetDrive(0x05) + 0x1C);
@@ -55,7 +55,11 @@ int YS::ANTI_FORM::CheckForm(char* player, int form)
 
             *reinterpret_cast<int*>(_antiFormRam + 0x04) = _calcAntiPoints;
 
-            return 5;
+            if (!YS::SACRIFICE::GetFormStatus(0x05))
+                return 5;
+
+            else
+                return form;
         }
     }
 
